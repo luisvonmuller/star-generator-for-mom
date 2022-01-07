10625047fdd2c6b30e490ac8857a637bb1594456
Luís Von Muller <luisvmuller@icloud.com>
2022-01-07 11:10:31 -0300

    Multi Thread implemented 😎

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,18 +1,22 @@
-use crate::Student;
-use std::sync::Arc;
+use crate::{Student, Students};
 use std::{error::Error, fs::File, io::BufRead, io::BufReader};
 
-pub fn students_collections(reader: BufReader<&File>) -> Result<Arc<Vec<Student>>, Box<dyn Error>> {
-    let mut students_collection = Vec::new();
+pub fn students_collections(
+    reader: BufReader<&File>,
+    students_collection: Students,
+) -> Result<Students, Box<dyn Error>> {
     for student in reader.lines() {
         let student = student?.trim().to_owned();
-        let _student = student.split('-').collect::<Vec<&str>>();
+        let student = student.split('-').collect::<Vec<&str>>();
 
-        students_collection.push(Student {
-            name: _student[0].to_owned(),
-            book: _student[1].to_owned(),
-        });
+        /* Change to mutex */
+        if let Ok(mut col) = students_collection.lock() {
+            col.push(Student {
+                name: student[0].to_owned(),
+                book: student[1].to_owned(),
+            });
+        }
     }
 
-    Ok(Arc::new(students_collection))
+    Ok(students_collection)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use std::sync::Arc;
+extern crate jemallocator;
+extern crate num_cpus;
+extern crate threadpool;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+use std::sync::{Arc, Mutex};
 use std::{error::Error, fs::File, io::BufReader};
-use tokio_stream::{self as stream, StreamExt};
+// use tokio_stream::{self as stream, StreamExt};
 
 pub mod drawer;
 pub mod helpers;
+
+/* Pseudo Types */
+pub type Students = Arc<Mutex<Vec<Student>>>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Student {
@@ -15,6 +25,13 @@ pub struct Student {
 async fn main() -> Result<(), Box<dyn Error>> {
     let file_information = File::open("./sample.txt")?;
     let file_reader = BufReader::new(&file_information);
+
+    let students_collections: Students = Arc::new(Mutex::new(Vec::new()));
+    if let Ok(student_collection_populated) =
+        helpers::students_collections(file_reader, students_collections)
+    {
+        process_stars(student_collection_populated).await;
+    }
 
     /* Ok Then here I'll have to Use and Arc because Students_collection must be borrowed
     with some 'static life time, and to be shared between threads in a safe way...
@@ -29,19 +46,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
     while increasing a reference count. When the last Arc pointer to a given allocation is destroyed,
     the value stored in that allocation (often referred to as “inner value”) is also dropped.
     Shared references in Rust disallow mutation by default, and Arc is no exception: you cannot generally obtain a mutable reference
-    to something inside an Arc. If you need to mutate through an Arc, use Mutex, RwLock, or one of the Atomic types.
+    to something inside an Arc.
+     If you need to mutate through an Arc, use Mutex, RwLock, or one of the Atomic types.
     */
-    let mut students_collection = stream::iter(helpers::students_collections(file_reader)?);
-    /*!I think the arc must be created here within a mutex, by that I mean giving main threads ownership to later itarete overself.think */
-    /* I think this will block thread spawing ? */
-    while let Some(student) = students_collection.next().await {
-        tokio::spawn(async move {
-            println!("Spawning Thread...");
-            drawer::process(&student).await;
-        });
 
-        return Ok(());
-    }
+    //     let mut students_collection = stream::iter(helpers::students_collections(file_reader)?);
+    //     /*!I think the arc must be created here within a mutex, by tha st I mean giving main threads ownership to later itarete overself.think */
+    //     /* I think this will block thread spawing ? */
+    //     while let Some(student) = students_collection.next().await {
+    //         tokio::spawn(async move {
+    //             println!("Spawning Thread...");
+    //             drawer::process(&student).await;
+    //         });
+    //
+    //         return Ok(());
+    //     }
 
     Ok(())
+}
+
+pub async fn process_stars(students_collection: Students) {
+    use threadpool::ThreadPool;
+
+    if let Ok(gurizada) = students_collection.lock() {
+        let arco_iris = num_cpus::get() * 2;
+        let piscinao_de_ramos = ThreadPool::new(arco_iris);
+        let gurizada_pro_churras = gurizada.len();
+
+        for _ in 0..gurizada_pro_churras {
+            piscinao_de_ramos.execute(move || println!("Timbum 😎"));
+        }
+    };
+
+    println!("So far, so Great!");
 }
